35f8a9838a8f9c26d02167776afbfb8580cf3c4b
abvrak <bitruut@gmail.com>
2025-03-10 21:34:48 +0100

    Add centered card with weather info

--- weatherapplication/views.py
+++ weatherapplication/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 import urllib.request
+import json
 def index(request):
-    import json
-    import requests
-
     city = "Lublin"
 
-    api_request = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
-                                    city + '&units=metric&appid=774f08ce17fe66123b6b05f685240b05').read()
     try:
+        api_request = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                             city + '&units=metric&appid=774f08ce17fe66123b6b05f685240b05').read()
         api = json.loads(api_request)
     except Exception as e:
         api = "Error"
-    return render(request, 'index.html', {'api': api})
+    return render(request, 'index.html', {'api': api, 'city':city})
 def about(request):
     return render(request, 'about.html', {})
